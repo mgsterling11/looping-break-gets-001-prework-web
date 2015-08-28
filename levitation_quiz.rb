50fa67db53b1d743507ee82c430d244aaff45831
mgsterling11 <m.g.sterling11@gmail.com>
2015-08-28 13:43:05 -0400

    Test 4, remote build failing, trying with gets.chomp.downcase

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -2,15 +2,11 @@
 def levitation_quiz
   loop do
     	puts "What is the spell that enacts levitation?"
-      answer = gets.chomp
-      break if answer == "Wingardium Leviosa"
+      answer = gets.chomp.downcase
+      break if answer == "wingardium leviosa"
   end
-
   puts "You passed the quiz!"
-
 end
-
-
 
 levitation_quiz
 
